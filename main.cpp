9f376521b36659a3e9787062616c56b10cabc6b0
jakestringfellow <stringfellow.j@northeastern.edu>
2023-03-25 23:55:20 -0400

    Added 3D point calibration image setup

--- main.cpp
+++ main.cpp
@@ -36,10 +36,23 @@ int main(int argc, char *argv[]) {
 
     // Initialize frame, will be streamed from the camera
     cv::Mat frame;
-    
+
+    int calib_counter = 1;
+
+    cv::Vec3f point_set;
+    std::vector<std::vector<cv::Vec3f>> point_list;
+    std::vector<std::vector<cv::Point2f>> corner_list;
+
+    // Boolean switch to make sure the points are calculated only once 
+    bool points_calculated = false; 
+
     for(;;) {
+	// Only create 5 calibration images
+	if (calib_counter > 5) {
+	    calib_counter = 1;
+	}
+
         *capdev >> frame; // get a new frame from the camera, treat as a stream
-//        cv::Mat threshold = preprocess_threshold(frame);
 
 	cv::Mat grayscaleFrame;
 	
@@ -47,23 +60,28 @@ int main(int argc, char *argv[]) {
 	cv::cvtColor(frame, grayscaleFrame, cv::COLOR_BGR2GRAY);
 
         std::vector<cv::Point2f> corner_set;
-	cv::Size patternsize(9,6);
+	std::vector<cv::Vec3f> point_set;
+
+	int pattern_columns = 9;
+	int pattern_rows = 6;
+
+	cv::Size patternsize(pattern_columns,pattern_rows);
+	bool cornersFound = false;
 	
 	// Without flags the fps slows considerably
-	bool cornersFound = cv::findChessboardCorners(grayscaleFrame, patternsize, corner_set, cv::CALIB_CB_ADAPTIVE_THRESH 
+	cornersFound = cv::findChessboardCorners(grayscaleFrame, patternsize, corner_set, cv::CALIB_CB_ADAPTIVE_THRESH 
 						      + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK);
 
-	std::cout << corner_set.size() << std::endl;
+	//std::cout << corner_set.size() << std::endl;
 
 
 	if (cornersFound) {
-	    cv::cornerSubPix(grayscaleFrame, corner_set, cv::Size(1,1), cv::Size(-1,-1), 
+	    cv::cornerSubPix(grayscaleFrame, corner_set, cv::Size(11,11), cv::Size(-1,-1), 
 			 cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 30, 0.1));
 	}
 
 	cv::drawChessboardCorners(frame, patternsize, cv::Mat(corner_set), cornersFound);
 	
-
 
         // If the frame is empty, alert the user, break the loop
         if( frame.empty() ) {
@@ -73,7 +91,6 @@ int main(int argc, char *argv[]) {
 	
         else {
             cv::imshow("Live Video", frame);
-	    //cv::imshow("Threshold Video", threshold_CU);
 	}
     
         // see if there is a waiting keystroke
@@ -82,6 +99,47 @@ int main(int argc, char *argv[]) {
         // If that keystroke is s, take a screenshot
         if( key == 's') {
             cv::imwrite("screenshot.jpg", frame);
+
+	    // Calculate the points if they haven't been calculated already
+	    if (!points_calculated) {
+                // Create the point set, should be the same for each calibration image  
+                // In the form of top left = (0,0,0), to the right is (1,0,0), row below is (0,-1,0)
+                for (int i=0; i<corner_set.size(); i++) {
+                    for (int c=0; c<pattern_columns; c++) {
+                        for (int r=0; r<pattern_rows; r++) {
+                            // For a planar pattern like a chessboard, set z to zero
+                            // Since we are going down in y-value, it is negative
+                            cv::Vec3f point = cv::Vec3f(c,0-r,0);
+                            point_set.push_back(point);
+                        }
+                    }
+                }
+                // Set the boolean to true
+                points_calculated = true;
+            }
+
+		
+	    if (cornersFound && calib_counter < 6) {
+		// Counter string between 1 and 5 for image naming
+	        std::string counterString = std::to_string(calib_counter);
+	        std::string calibImgName = "calib"+counterString+".jpg";
+
+		// Save the image
+	        cv::imwrite(calibImgName, frame);
+
+	        // Set the index of the vectors to the sets for the calibration images chosen
+		point_list.push_back(point_set);
+		corner_list.push_back(corner_set);
+
+		// Alert the user to calibration being complete
+		if (calib_counter == 5) {
+		    std::cout << "Calibration complete." << std::endl;
+		    std::cout << "Any additional snapshots will not be recorded." << std::endl;
+		}
+		
+		// Increment the counter
+		++calib_counter;
+	    }
 	}
         // If that keystroke is q, quit the program
         else if( key == 'q') {
